d123625ccf4b05f711c1ed0ebae57ac8dd37a246
Gunka Kseniya <gunyasha1990@yandex.ru>
2025-09-01 00:30:45 +0300

    ConstructorTest changed, NavigationTest deleted after review

--- src/test/java/tests/ConstructorTest.java
+++ src/test/java/tests/ConstructorTest.java
@@ -1,37 +1,57 @@
 package tests;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import pageobject.MainPage;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertEquals;
 
 public class ConstructorTest extends BaseTest {
 
     private final String baseUrl = "https://stellarburgers.nomoreparties.site/";
+    private MainPage mainPage;
+
+    @Before
+    public void setUp() {
+        open(baseUrl);
+        mainPage = new MainPage();
+    }
+
+    @After
+    public void tearDown() {
+        closeWebDriver();
+    }
 
     @Test
     public void tabSwitch() {
-        open(baseUrl);
-        MainPage mainPage = new MainPage();
+        checkTab("Булки");
+        checkTab("Соусы");
+        checkTab("Начинки");
+    }
 
-        mainPage.clickTab("Булки");
-        assertEquals("Булки", mainPage.getCurrentTabText());
-
-        mainPage.clickTab("Соусы");
-        assertEquals("Соусы", mainPage.getCurrentTabText());
-
-        mainPage.clickTab("Начинки");
-        assertEquals("Начинки", mainPage.getCurrentTabText());
+    private void checkTab(String tabName) {
+        mainPage.clickTab(tabName);
+        mainPage.tabs.findBy(text(tabName))
+                .shouldBe(visible)
+                .shouldHave(cssClass("tab_tab_type_current__2BEPc"), Duration.ofSeconds(3));
     }
 
     @Test
     public void returnToConstructorByLogo() {
-        open(baseUrl);
-        MainPage mainPage = new MainPage();
         mainPage.clickLoginButton();
         mainPage.clickLogo();
         assertTrue("Отображение конструктора после клика по лого", mainPage.isConstructorVisible());
+    }
+
+    @Test
+    public void defaultBunsTabIsActive() {
+        mainPage.tabs.findBy(text("Булки"))
+                .shouldHave(cssClass("tab_tab_type_current__2BEPc"), Duration.ofSeconds(2));
     }
 }
